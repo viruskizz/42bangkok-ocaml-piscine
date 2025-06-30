b797d1c5d6169fab34745fbcb88b171cfffc3b89
viruskizz <kizzaraiva@gmail.com>
2025-07-01 01:40:40 +0700

    m01-ex09

--- m01/ex09/leibniz_pi.ml
+++ m01/ex09/leibniz_pi.ml
@@ -1,1 +1,14 @@
-let leibniz_pi 
+let leibniz_pi () =
+  let arctan = atan 1.0 in
+  let epsilon = 1e-6 in  (* minimum delta *)
+  let rec aux i (sum: float) =
+    let term = ((-1.) ** float_of_int i) /. float_of_int((2 * i + 1)) in
+    let new_sum = sum +. term in
+    (* if abs_float(new_sum -. delta) < epsilon then *)
+    if (new_sum -. arctan < epsilon) && (arctan -. new_sum < epsilon) then
+      4. *. new_sum
+    else
+      aux (i + 1) new_sum
+  in
+  aux 0 0.0
+;;
