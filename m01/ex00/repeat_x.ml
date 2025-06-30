34ee8555691b56e2cfa2c1a6cb87d78eea2a3624
viruskizz <kizzaraiva@gmail.com>
2025-06-30 22:44:40 +0700

    m01-ex01

--- m01/ex00/repeat_x.ml
+++ m01/ex00/repeat_x.ml
@@ -1,13 +1,13 @@
 let repeat_x n =
-    if n < 0 then
-        "Error"
-    else
-        let x = "" in
-        let rec aux n =
-            if n > 0 then
-                "x" ^ aux (n - 1)
-            else
-                x
-        in
-        aux n
+  if n < 0 then
+    "Error"
+  else
+    let x = "" in
+    let rec aux n =
+      if n > 0 then
+        "x" ^ aux (n - 1)
+      else
+        x
+    in
+    aux n
 ;;
