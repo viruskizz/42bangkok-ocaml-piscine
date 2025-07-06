b0dc187baf36dbff4b3418e1aee801ccea997888
Thitiwut Somsa <tsomsa@42bangkok.com>
2025-07-06 18:18:50 +0700

    m01-ex08: change to tail recursive

--- m01/ex08/ft_sum.ml
+++ m01/ex08/ft_sum.ml
@@ -1,7 +1,8 @@
 let rec ft_sum (fn: int -> float) (i: int) (n: int) : float =
-  if i > n then
-    nan
-  else if i = n then
-    fn i
-  else
-    fn i +. ft_sum fn (i + 1) n
+  let rec aux i acc =
+    if i > n then
+      acc
+    else
+      aux (i + 1) (acc +. fn i)
+  in
+  aux i 0.0
